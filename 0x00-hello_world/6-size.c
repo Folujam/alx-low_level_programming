b27198297df99804ebd24f148b9eda5844792b9b
folujam <olusway@gmail.com>
2023-09-08 05:19:25 +0000

    tes code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of an interger is %li\n", sizeof(int));
-	printf("size of an character is %li\n", sizeof(char));
-	printf("size of an short is %li\n", sizeof(short));
-	printf("size of an long double is %li\n", sizeof(long));
-	printf("size of an float is %li\n", sizeof(float));
+	printf("size of an interger is %li-byte(s)\n", sizeof(int));
+	printf("size of an character is %li-byte(s)\n", sizeof(char));
+	printf("size of an short is %li-byte(s)\n", sizeof(short));
+	printf("size of an long double is %li-byte(s)\n", sizeof(long));
+	printf("size of an float is %li-byte(s)\n", sizeof(float));
 	return (0);
 }
