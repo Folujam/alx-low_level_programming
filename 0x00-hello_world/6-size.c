3497172d03f950c5014fae47c46feefaf102a67d
folujam <olusway@gmail.com>
2023-09-08 07:05:59 +0000

    check check

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("Size of an interger is %li byte(s)\n", sizeof(int));
 	printf("Size of a character is %li byte(s)\n", sizeof(char));
-	printf("Size of a short is %li byte(s)\n", sizeof(short));
-	printf("Size of a long double is %li byte(s)\n", sizeof(long));
+	printf("size of a long long is %li byte(s)\n", sizeof(long long));
+	printf("Size of a long int is %li byte(s)\n", sizeof(long));
 	printf("Size of a float is %li byte(s)\n", sizeof(float));
 	return (0);
 }
