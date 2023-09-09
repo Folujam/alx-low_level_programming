473bc2eeb5154b97d17f13766d1ee3c46c038b3b
folujam <olusway@gmail.com>
2023-09-09 02:08:24 +0000

    lets gooooooooo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,18 @@
 
 int main(void)
 {
-		int n = '0';
-		
-		while (n <= '9')
+	int n = '0';
+	
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9');
 		{
-			putchar(n);
-			if (n != '9')
-			{
-				putchar(',');														putchar(' ');													}
-			n++;
+			putchar(',');
+			putchar(' ');
 		}
-		putchar('\n');
-
-		return (0);
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
