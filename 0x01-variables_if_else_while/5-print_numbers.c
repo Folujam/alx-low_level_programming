08550279fde96da2a85bf653fb485fb725943e11
folujam <olusway@gmail.com>
2023-09-09 01:47:22 +0000

    Jesus

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- * main- Entry point, where the program begins
- *
- * Return: Always 0 (Success), where it ends
- *
+ * main- Entry point,
+ * Return: Always 0 (Success), 
  */
 
 int main(void)
 {
 	int digit = 0;
-	
+
 	while (digit < 10);
 	{
 		printf("%i", digit);
