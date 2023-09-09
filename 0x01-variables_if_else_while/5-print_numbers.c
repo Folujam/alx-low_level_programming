26d2b6a7155a8b1b4b506b411ad41b0b079474db
folujam <olusway@gmail.com>
2023-09-09 00:03:35 +0000

    lets gooooo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * main- Entry point, where the program begins
+ *
+ * Return: Always 0 (Success), where it ends
+ *
+ */
+
+int main(void)
+{
+	int digit = 0;
+	
+	while (digit < 10);
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	putchar('\n');
+
+		return (0);
+}
