4eb5119559bc6cf85e9b56e6909cdf98494d2a4e
folujam <olusway@gmail.com>
2023-09-09 14:02:44 +0000

    sec dead exc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of an interger is %zu byte(s)\n", sizeof(int));
-	printf("Size of a character is %zu byte(s)\n", sizeof(char));
-	printf("size of a long long is %zu byte(s)\n", sizeof(long long));
-	printf("Size of a long int is %zu byte(s)\n", sizeof(long));
-	printf("Size of a float is %zu byte(s)\n", sizeof(float));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
