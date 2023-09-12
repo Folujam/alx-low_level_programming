8fe135af5f50e772e36cf8d04309a3ad9826d791
folujam <olusway@gmail.com>
2023-09-12 21:21:40 +0000

    jjj

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main"
+
+/**
+  *print_alphabet_x10 - entry point
+  *Return: Always 0 (Success)
+  */
+
+void print_alphabet_x10(void)
+{
+	chai x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	putchar('\n');
+}
+/*lets print x10 acc jenny*/
+void print_alpabet_x10(void)
+{
+	int x;
+
+	for(x = 0; x <= '10'; x++)
+	{
+		print_alphabet();
+	}
+}
