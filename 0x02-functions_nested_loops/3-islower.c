3050f069f8cb20cb896f30babe2b5c15b8471edf
folujam <olusway@gmail.com>
2023-09-12 22:26:15 +0000

    jjj

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *_islower - entry point
+  *Return: always 0 (Success)
+  */
+
+int _islower(int c)
+{
+	if (c => 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
