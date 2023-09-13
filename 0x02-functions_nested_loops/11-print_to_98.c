420bfacf5dd3903bd623e6b2e1fbb9d3ee365eca
folujam <olusway@gmail.com>
2023-09-13 19:48:08 +0000

    heaven

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_to_98 - entry point
+ *@q: num to print
+ *Return: always 0 (success)
+ */
+void print_to_98(int q)
+{
+	if (n <= 98)
+	{
+		for (n <= 98);
+		{
+			if (n == 98)
+			{
+				_putchar(n + '0');
+				_putchar('\n');
+				break;
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+		}
+	}
+}
