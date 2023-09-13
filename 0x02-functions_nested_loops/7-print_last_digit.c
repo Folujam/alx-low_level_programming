096c14178f926ad1631edda0ea84e11d11fe77e0
folujam <olusway@gmail.com>
2023-09-13 02:45:14 +0000

    ho

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_last_digit - entry point
+ *@c: decleared variable
+ *Return: only last num
+ */
+int print_last_digit(int c)
+{
+	int t;
+
+	t = c % 10;
+	while (t < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('1');
+		return (1);
+	}
+}
+
+
