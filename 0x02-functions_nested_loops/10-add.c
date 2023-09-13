d590f00119dffb79d252aa45e5a8d3d8550d29ab
folujam <olusway@gmail.com>
2023-09-13 18:28:06 +0000

    sum it

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - Entry point
+ *@w1: first int
+ *@w2: second int
+ *return: result
+ */
+int add(int w1, int w2)
+{
+	int result;
+
+	result = w1 + w2;
+	return (result);
+}
