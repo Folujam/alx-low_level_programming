8aef15a8560562426b03da5c594979bae0aec9b5
folujam <olusway@gmail.com>
2023-09-13 22:39:44 +0000

    Holy Ghost

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,30 +12,30 @@ void print_times_table(int n)
 	int wxw;
 	int promax;
 
-	if (n >= 0 && n <= 15) //range in which to print
+	if (n >= 0 && n <= 15) /*range in which to print*/
 	{
-		for (q = 0; q <= n; q++) //numba-q starts from 0 and ends at 'n'
+		for (q = 0; q <= n; q++) /*numba-q starts from 0 and ends at 'n'*/
 		{
-			_putchar('0'); //prints numba
-			for (wxw = 1; wxw <= n; wxw++) //multiplicable numba
+			_putchar('0'); /*prints numba*/
+			for (wxw = 1; wxw <= n; wxw++) /*multiplicable numba*/
 			{
 				_putchar(',');
 				_putchar(' ');
-				promax = q * wxw; //find result: numba by multiplier
-				if (promax <= 99) //double spacer
+				promax = q * wxw; /*find result: numba by multiplier*/
+				if (promax <= 99) /*double spacer*/
 					_putchar(' ');
 				if (promax <= 9)
 					_putchar(' ');
-				if (promax >= 100) //i'm still thinking here, wow!
+				if (promax >= 100) /*i'm still thinking here, wow!*/
 				{
 					_putchar((promax / 100) + '0');
-					_putchar(((promax / 10) % 10) + '0'); //double numbas
+					_putchar(((promax / 10) % 10) + '0'); /*double numbas*/
 				}
 				else if (promax <= 99 && promax >= 10)
 				{
 					_putchar((promax / 10) + '0');
 				}
-					_putchar((promax % 10) + '0'); //3ple numbas
+					_putchar((promax % 10) + '0'); /*3ple numbas*/
 			}
 			_putchar('\n');
 		}
