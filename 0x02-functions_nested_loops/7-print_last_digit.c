19aa3b9dfb4218aa5a25a1ba72cfbb7c215aed41
folujam <olusway@gmail.com>
2023-09-13 15:54:53 +0000

    24hours

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,15 +12,10 @@ int print_last_digit(int c)
 	t = c % 10;
 	if (t < 0)
 	{
-		_putchar(-1 + '0');
-		return (-1);
+		t = t * -1;
 	}
-	else
-	{
-		_putchar(1 + '0');
-		return (1);
-	}
-	return (0);
+		_putchar(t + '0');
+		return (t);
 }
 
 
