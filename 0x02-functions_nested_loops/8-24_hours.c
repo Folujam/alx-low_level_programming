d04b709d6405eb2cef7e61e0f3bc4ca68bedec1f
folujam <olusway@gmail.com>
2023-09-13 15:35:48 +0000

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,25 +7,27 @@
  */
 void jack_bauer(void)
 {
-	int u;
-	int i;
-	int o;
-	int p;
+	int u = 0;
+	int i = 0;
+	int o = 0;
+	int p = 0;
 
-	for (u = 48; u <= 2; u++)
+	for (u <= 2; u++)
 	{
-		for (i = 48; i <= 3; i++)
+		for (i <= 9; i++)
 		{
-			_putchar('u');
-			_putchar('i');
-			_putchar(':');
+			if ((u <= 1 && i <= 9) || (u <= 2 && i <= 3))
 			{
-				for (o = 48; o <= 5; o++)
+				for (o <= 5; o++)
 				{
-					for (p = 48; p <= 9; p++)
+					for (p <= 9; p++)
 					{
+						_putchar('u');
+						_putchar('i');
+						_putchar(':');
 						_putchar('o');
 						_putchar('p');
+						_putchar('\n');
 					}
 				}
 			}
