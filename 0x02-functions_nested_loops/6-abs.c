708b6463845f40dee8ab2cea41d7a21a68eaae17
folujam <olusway@gmail.com>
2023-09-13 00:35:18 +0000

    ho

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
- *
+ *_abs- entry point
+ *@c: decleared variable
+ *Return: always 0 success
+ */
+int _abs(int);
+{
+	if (g < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
