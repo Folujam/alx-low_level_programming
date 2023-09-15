f669b91cdca64a7be931acd0a994579ba86964a9
folujam <olusway@gmail.com>
2023-09-15 11:44:41 +0000

    hope it works

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,4 +7,9 @@
 void print_most_numbers(void)
 {
 	for (q = 0; q <=9; q++)
-
+	{
+		if (q != 50 && q != 52)
+		_putchar(q + '0');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *more_numbers - print 0 to 14, x10
+  *Return: 0
+  */
+void more_numbers(void)
+{
+	int l;
+	int h;
+
+	for (l = 0; l <= 9; l++)
+	{
+		for (h = 0; h <= 14; h++)
+		{
+			if (h > 9)
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
