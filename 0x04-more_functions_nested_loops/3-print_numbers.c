c9fdc8da6c42163bf19029a8fd73428b00c5b150
folujam <olusway@gmail.com>
2023-09-15 02:17:59 +0000

    result

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *print_numbers - prints natural numbers
+  *Return: 0
+  */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0');
+		return (0);
+	}
+	_putchar ('\n');
+}
+
