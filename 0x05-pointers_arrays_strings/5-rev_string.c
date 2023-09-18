5d1d9070b2a94daa0b62d940102e8a170bdc3fcc
folujam <olusway@gmail.com>
2023-09-18 20:54:10 +0000

    hello

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,22 +7,26 @@
  */
 void rev_string(char *s)
 {
-	int d;
+	int d; /*count*/
 	int f;
-	int
+	int ind1 = 0; /*index 1*/
+	int ind2; /*index 2*/
+	char z;
+	char *u = s;
 
 	d = 0;
-	while (*s != '\0')
+	while (*u != '\0')
 	{
 		s++;
 		d++;
 	}
-	f = d - 1;
-	g = 0
-	while (f >= 0)
+	f = d - 1; /*lenght of string std*/
+	while (ind1 < (f / 2) + 1)
 	{
-		_putchar(s[d]);
-		f--;
+		ind2 = (d - ind1);
+		z = s[ind1];
+		s[ind1] = s[ind2];
+		s[ind2] = z;
+		f++;
 	}
-	_putchar('\n');
 }
