7ae5f566b0d8c33d39482f3d5529fbddcd508ab6
folujam <olusway@gmail.com>
2023-09-20 01:19:04 +0000

    joj

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,17 +8,13 @@
 void puts2(char *str)
 {
 	int index = 0;
-	int q =  index - 1;
 
 	while (str[index] != '\0')
 	{
-		index++;
-	}
-
-	while (q != '\0')
-	{
 		if (index % 2 == 0)
-		_putchar(str[q]);
+		{
+			_putchar(str[index]);
+		}
 		index++;
 	}
 	_putchar('\n');
