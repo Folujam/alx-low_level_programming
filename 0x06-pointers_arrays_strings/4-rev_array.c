e62914df5747b5a847d5048dec5374b6226cf5e1
folujam <olusway@gmail.com>
2023-09-21 18:59:19 +0000

    test task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *reverse_array - reverses array content
+  *@a: declared variable
+  *@n: declared variable
+  *Return: void.
+  */
+void reverse_array(int *a, int n)
+{
+	int b = 0;
+	int lenght = n - 1;/*length=number of elements(n) - null*/
+	int temp;
+
+	while (b < (lenght / 2))
+	{
+		temp = a[b];/*temp is needed to store hanging element*/
+		a[b] = a[lenght - 1 - b];
+		a[lenght - 1 - b] = temp;
+		b++;
+	}
+}
