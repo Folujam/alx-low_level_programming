b9ebaa23d00bebc221b9c9ea9fdf33ed29151d24
folujam <olusway@gmail.com>
2023-09-21 17:56:56 -0700

    task 6 runs but line 27 too long 82>80

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  *strng_toupper - entry point
+  *@ups: declared variable
+  *Return: ups
+  */
+char strng_toupper(char ups)
+{
+	if (ups >= 'a' && ups <= 'z')
+	{
+		ups = ups - 32;
+	}
+	return (ups);
+}
+/**
+ **cap_string - caps each word
+ *@capa: declared variable
+ *Return: capa
+ */
+char *cap_string(char *capa)
+{
+	int a;
+
+	for (a = 0; capa[a] != '\0'; a++)
+	{
+		if (a == 0 || capa[a - 1] == ' ' || capa[a - 1] == '\n' || capa[a - 1] == '\t')
+		{
+			capa[a] = strng_toupper(capa[a]);
+		}
+	}
+	return (capa);
+}
