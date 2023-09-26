2547f19dfbdedeae2bccb880125367685358d334
folujam <olusway@gmail.com>
2023-09-26 13:24:14 +0000

    g runs

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_diagsums - numbers diagonal
+  *@a: declared ptr
+  *@size: declared var
+  *Return: void
+  */
+void print_diagsums(int *a, int size)
+{
+	/*from example there are two sums for each*/
+	/*initialize both sums*/
+	int sum1 = 0;
+	int sum2 = 0;
+
+	int i = 0;
+
+	while (i <= (size - 1))
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - 1 - i)];
+		i++;
+	}
+	printf("sum1 %d, sum2 %d\n", sum1, sum2);
+}
+
