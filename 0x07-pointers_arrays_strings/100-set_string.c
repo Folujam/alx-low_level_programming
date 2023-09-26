af8bbd93fabd48ffdedca3d6678fffcf29dca2b1
folujam <olusway@gmail.com>
2023-09-26 15:39:48 +0000

    100 runs

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *set_string - pointer to pointer
+  *@s: pointer to pointer variable
+  *@to: pointer varible
+  *Return: empty/void
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
