3994835468972d2d365ed82aaf0fa6e2d618fbe0
folujam <olusway@gmail.com>
2023-09-26 20:41:55 +0000

    j

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,12 +4,10 @@
 /**
  *print_to_98 - entry point
  *@n: variable if under 98
- *@q: variable if over 98
  *Return: always 0 (success)
  */
 void print_to_98(int n)
 {
-
 	if (n <= 98)
 	{
 		for (; n <= 98; n++)
@@ -21,19 +19,17 @@ void print_to_98(int n)
 				printf(", ");
 			}
 		}
-		_putchar('\n');
 	}
 	else if (n >= 98)
 	{
 		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-
-			 if (n != 98)
-			 {
-				printf(", ");
-			 }
 		}
-		_putchar('\n');
+		if (n != 98)
+		{
+			printf(", ");
+		}
 	}
+	printf("\n");
 }
