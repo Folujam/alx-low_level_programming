85e46f6d35f275c953a44ac6a19e34d798df83d9
folujam <olusway@gmail.com>
2023-09-26 19:08:43 +0000

    these guys are making me test this

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,41 @@
 #include "main.h"
 /**
  *print_to_98 - entry point
- *@q: num to print
+ *@n: variable if under 98
+ *@q: variable if over 98
  *Return: always 0 (success)
  */
-void print_to_98(int q)
+void print_to_98(int n)
 {
-	if (q <= 98)
+	int q;
+
+	if (n <= 98)
 	{
-		for (q = 0; q <= 98; q++)
+		for (n = 0; n <= 98; n++)
 		{
-			if (q == 98)
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+
+			if (n != 98)
 			{
-				_putchar((q / 10) + '0');
-				_putchar((q % 10) + '0');
-				_putchar('\n');
-				break;
+				_putchar(',');
+				_putchar(' ');
 			}
-			else
-			{
-				_putchar((q / 10) + '0');
-				_putchar((q % 10) + '0');
-			}
+		}
+		_putchar('\n');
+	}
+	else if (n > 98)
+	{
+		for (q = n; q >= 98; q--)
+		{
+			_putchar((q / 10) + '0');
+			_putchar((q % 10) + '0');
+
+			 if (n != 98)
+			 {
+				_putchar(',');
+				_putchar(' ');
+			 }
 		}
 	}
 }
