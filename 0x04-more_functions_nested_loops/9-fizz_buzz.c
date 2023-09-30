ba1a1df82dd45a9f3bb96fd424883435feb5570c
ogofoluwa <olusway@gmail.com>
2023-09-30 13:41:13 +0000

    all9 run

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,18 +9,22 @@ int main(void)
 	int a;
 
 	for (a = 1; a <= 100; a++)
+		/*multiples of both 3&5*/
 		if (a % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
+		/*multiples of 5*/
 		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
 		}
+		/*multiples of 3*/
 		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
 		}
+		/*all other numbers*/
 		else
 		{
 			printf("%d ", a);
