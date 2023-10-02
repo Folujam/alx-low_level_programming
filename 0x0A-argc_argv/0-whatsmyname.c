f0c4fb130da702e1bb6033f8c8e70e622fac55cc
ogofoluwa <olusway@gmail.com>
2023-10-02 22:55:30 +0000

    task 0 runs

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - prints its name
+  *@argc: argument count
+  *@argv: argument vector/pointer
+  *Return: int
+  */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
