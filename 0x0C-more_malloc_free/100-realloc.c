944ab8c53417ae84f2b342a60cc36aff9e21c0bb
ogofoluwa <olusway@gmail.com>
2023-10-08 16:25:48 +0000

    h2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int *ptr2;
+	void *ptr2;
 	unsigned int a;
 
 	if (ptr == 0)
@@ -29,13 +29,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size != old_size)
 	{
-		ptr2 = malloc(new_size + 1);
+		ptr2 = malloc(new_size);
 		if (ptr2 == 0)
 			return (0);
 		a = 0;
 		while (a < new_size && a < old_size)
 		{
-			ptr2[a] = ((unsigned int *)ptr)[a];
+			ptr2[a] = ptr[a];
 			a++;
 		}
 		free(ptr);
