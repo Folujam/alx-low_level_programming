7c9f18f7e3454b5777bd764e4e607fa4d4cd324c
ogofoluwa <olusway@gmail.com>
2023-10-10 23:34:45 +0000

    dog.h updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,5 +15,11 @@ typedef struct dog
 } dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 #endif
