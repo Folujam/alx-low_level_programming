94154b58d783212b63000256642baeda9fef8fb7
ogofoluwa <olusway@gmail.com>
2023-10-19 21:14:22 +0000

    4 runs, test

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  *free_list - function that frees a list_t list
+  *@head: pointr
+  *Return: void
+  */
+void free_list(list_t *head)
+{
+	char *temp;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
