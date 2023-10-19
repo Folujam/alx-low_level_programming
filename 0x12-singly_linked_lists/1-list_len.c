ae34b4e412bef58969ea1fdea09175fb0674c697
ogofoluwa <olusway@gmail.com>
2023-10-19 15:59:52 +0000

    1 runs

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
-  *
+  *list_len -function that returns the number of elements in a linked list_t.
+  *@h: pointer
+  *Return: counter
   */
 size_t list_len(const list_t *h)
 {
