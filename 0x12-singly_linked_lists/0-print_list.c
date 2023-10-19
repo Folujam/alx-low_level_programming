2d10f0cb2723cc690aec801923264c074501a5ce
ogofoluwa <olusway@gmail.com>
2023-10-19 15:17:54 +0000

    task 0 runs, test checks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  *print_list - function that prints all the elements of a list_t list.
+  *@h: pointer to str in h field
+  *Return: counter
+  */
+size_t print_list(const list_t *h)
+{
+	int ctr = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n",h->len, h->str);
+		}
+		ctr++;
+		h = h->next;
+	}
+	return (ctr);
+}
