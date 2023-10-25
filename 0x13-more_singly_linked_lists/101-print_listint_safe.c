bd7ad2f08df0aa8c3219d3309d489d6788037494
ogofoluwa <olusway@gmail.com>
2023-10-25 00:59:34 +0000

    dont run, dont test

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,28 +16,27 @@ size_t print_listint_safe(const listint_t *head)
 		exit(98);
 	for (i = 0; hed != NULL; i++)
 	{
-		for (a = 0; a < size; a++)
+		darr[i] = hed;
+		if (darr[i] > 0 && darr[i] == darr[i - 1])
 		{
-			darr[i] = hed;
-			if (i > 0 && darr[i] == darr[i - 1])
-			{
-				printf("[%p] %d", i, hed->n);
-				break;
-			}
-			hed = hed->next;
-			else
-			{
-				darr[i] = hed->next;
-				printf("[%p] %d", i, hed->n);
-			}
-			if (hed == NULL)
+			printf("[%p] %d", i, hed->n);
+			break;
+		}
+		else
+		{
+			if (i == size)
 			{
 				size = 64;
-				darr = malloc(size * sizeof(listint_t *));
+				darr = realloc(size * sizeof(listint_t *));
 				if (darr == NULL)
+					free(darr);
 					exit(98);
 			}
+			darr[i] = hed->next;
+			printf("[%p] %d",darr[i], hed->n);
 		}
+		hed =hed->next;
 	}
 	free(darr);
 	return (i);
+}
