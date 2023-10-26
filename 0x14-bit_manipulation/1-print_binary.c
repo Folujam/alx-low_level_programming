7fd569f58f8a60398aadb01b837b603d7d5432be
ogofoluwa <olusway@gmail.com>
2023-10-26 20:03:18 +0000

    updated main.h, 1 runs

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  *print_binary - function that prints the binary representation of a number
+  *@n: u long int var
+  *Return: void
+  */
+void print_binary(unsigned long int n)
+{
+	int i, shift;
+	unsigned long int temp;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	shift = 0;
+	temp = n;
+	while (temp != 0)
+	{
+		temp = temp >> 1;
+		shift += 1;
+	}
+	for (i = shift - 1; i >= 0; i--)
+	{
+		if ((n & (1 << i)) != 0)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
