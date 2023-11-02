e7d822785328eaeaff019adc2f9c1fd4e74f6de1
ogofoluwa <olusway@gmail.com>
2023-11-02 14:05:51 +0000

    3cp incomp, updated main.h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
 	FILE *a2;
 	const char *file_from = argv[1], *file_to = argv[2];
-	char racter[1024];/*buffer to hold 1024 chars at a r&w*/
+	char acter[1024];/*buffer to hold 1024 chars at a r&w*/
 	size_t a, b; /*a = amount read, b = amount wrttn*/
 	int a3;
 
@@ -28,22 +28,60 @@ int main(int argc, char **argv)
 	a3 = open(file_to, O_CREAT | O_WRONLY, 0664);
 	if (a3 != -1)
 	{
-		while ((a = fread(racter, sizeof(char), 1024, a2)) > 0)
+		while (1)
 		{
-			b = write(a3, racter, sizeof(char) * a);
-			if (a > b)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
+			a = rdntst_a2(acter, a2, argv[1]);
+			b = write(a3, acter, sizeof(char) * a);
+			ab(a, b, argv[2]);
 		}
 	}
+	else
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
+	}
 	fclose(a2);
-	close(a3);
 	if (close(a3) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a3);
 		exit(100);
 	}
-	return (b);
+	return (0);
+}
+/**
+  *rdntst_a2 - test and treats a2 fd while reading it
+  *@acter: pointer to buffer
+  *@a2: open file
+  *Return: long int size_t
+  */
+size_t rdntst_a2(char *acter, FILE *a2, char **argv)
+{
+	size_t a;
+
+	a = fread(acter, sizeof(char), 1024, a2);
+	if (a < 1024)
+	{
+		if (feof(a2))
+			break;
+		if (ferror(a2))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", b);
+			exit(98);
+		}
+	}
+	return (a);
+}
+/**
+  *ab - test 2 para
+  *@a: para 1
+  *@b: para 2
+  *Return:
+  */
+void ab(size_t a, size_t b, char **argv)
+{
+	if (a > b)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
