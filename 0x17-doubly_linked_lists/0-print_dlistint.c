3dd5c3976911a49d97d06b2b4d39fc2b1297bb7b
folujam <olusway@gmail.com>
2023-12-07 22:03:26 +0100

    corrected files

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,17 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  *print_dlistint-  prints all the elements of a dlistint_t list
  *@h: pointer to struct
- *Return: size_t 
+ *Return: size_t
 */
 size_t print_dlistint(const dlistint_t *h)
 {
     int node_count = 0;
 
     while (h != NULL)
-    {   
-        printf("%d"; h->data);
+    {
+        printf("%d", h->n);
         node_count++;
         h = h->next;
     }
